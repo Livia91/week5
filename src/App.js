e9890b2d06c4623c616b88885d4f411485d60147
Livia91 <109625994+Livia91@users.noreply.github.com>
2022-12-31 20:50:15 +0000

    Update App.js
    added my GitHub address

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Weather app
-    </div>
-    
-  );
+export default function App() {
+  return <div className='App'>
+   <h1>Weather app</h1> 
+    <footer>
+      This project was create by Livia Pogar and is {""} 
+     <a href='https://github.com/Livia91/week5'>
+      open-sourced on Github
+    </a>
+    </footer>
+  </div>
 }
 
-export default App;
+
